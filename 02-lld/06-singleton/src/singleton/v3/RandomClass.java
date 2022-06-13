c5559317dfcffbdba9475639c7550871550028d4
Sanket <sanketwakhare@gmail.com>
2022-06-14 00:00:13 +0530

    singleton pattern - eager loading

---
+++ 02-lld/06-singleton/src/singleton/v3/RandomClass.java
@@ -0,0 +1,5 @@
+package singleton.v3;
+
+public class RandomClass {
+    Singleton instance = Singleton.getInstance();
+}
---
+++ 02-lld/06-singleton/src/singleton/v3/Singleton.java
@@ -0,0 +1,19 @@
+package singleton.v3;
+
+/**
+ * Eager Loading
+ * Cons:
+ * 1. no way to pass parameter to singleton constructor
+ * 2. cannot throw exception
+ */
+public class Singleton {
+
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
