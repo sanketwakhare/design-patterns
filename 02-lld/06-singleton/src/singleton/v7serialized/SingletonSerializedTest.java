cfda0985d8d7d148ca0bacd7c3e13cc114098381
Sanket <sanketwakhare@gmail.com>
2022-07-09 16:18:54 +0530

    singleton

---
+++ 02-lld/06-singleton/src/singleton/v7serialized/SingletonSerializedTest.java
@@ -0,0 +1,25 @@
+package singleton.v7serialized;
+
+import java.io.*;
+
+public class SingletonSerializedTest {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        SerializedSingleton instanceOne = SerializedSingleton.getInstance();
+        ObjectOutput out = new ObjectOutputStream(new FileOutputStream(
+                "filename.ser"));
+        out.writeObject(instanceOne);
+        out.close();
+        
+        //deserailize from file to object
+        ObjectInput in = new ObjectInputStream(new FileInputStream(
+                "filename.ser"));
+        SerializedSingleton instanceTwo = (SerializedSingleton) in.readObject();
+        in.close();
+        
+        System.out.println("instanceOne hashCode="+instanceOne.hashCode());
+        System.out.println("instanceTwo hashCode="+instanceTwo.hashCode());
+        
+    }
+
+}
