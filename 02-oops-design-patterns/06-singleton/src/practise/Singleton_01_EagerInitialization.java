cd43ddddb43cc433970c32cbe177d75bdefb67f9
sanketwakhare <sanketwakhare@gmail.com>
2022-10-13 22:58:52 +0530

    singleton: eager loading

--- 02-oops-design-patterns/06-singleton/src/practise/Singleton_01_EagerInitialization.java
+++ 02-oops-design-patterns/06-singleton/src/practise/Singleton_01_EagerInitialization.java
@@ -4,22 +4,22 @@ package practise;
 1. allocates memory to instance even when not invoked by client
 2. do not provide any exception handling mechanism
 */
-public class Singleton_01EagerInitialization {
+public class Singleton_01_EagerInitialization {
     public static void main(String[] args) {
-        Singleton instanceOne = Singleton.getInstance();
-        Singleton instanceTwo = Singleton.getInstance();
+        Singleton_v1 instanceOne = Singleton_v1.getInstance();
+        Singleton_v1 instanceTwo = Singleton_v1.getInstance();
         System.out.println(instanceOne.hashCode());
         System.out.println(instanceTwo.hashCode());
     }
 }
 
-class Singleton {
-    private static final Singleton instance = new Singleton();
+class Singleton_v1 {
+    private static final Singleton_v1 instance = new Singleton_v1();
 
-    private Singleton() {
+    private Singleton_v1() {
     }
 
-    public static Singleton getInstance() {
+    public static Singleton_v1 getInstance() {
         return instance;
     }
 }
